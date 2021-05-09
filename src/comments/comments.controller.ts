cdb85d9caed747c2e5b1836761076e846ca2e0ba
Mikhail Zolenko <mihail.zolenko@gmail.com>
2021-05-09 23:32:17 +0600

    added Comment owner guard

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -14,6 +14,7 @@ import { CommentsService } from './comments.service';
 import { CurrentUser } from '../guards/current-user.decorator';
 import { User } from '../users/user.entity';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CommentsOwnerGuard } from '../guards/comments-owner.guard';
 
 @ApiBearerAuth()
 @ApiTags('Comments')
@@ -36,16 +37,19 @@ export class CommentsController {
   }
 
   @Get(':id')
+  @UseGuards(CommentsOwnerGuard)
   findOne(@Param('id') id: string) {
     return this.commentsService.findOne(id);
   }
 
   @Put(':id')
+  @UseGuards(CommentsOwnerGuard)
   update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
     return this.commentsService.update(id, updateCommentDto);
   }
 
   @Delete(':id')
+  @UseGuards(CommentsOwnerGuard)
   delete(@Param('id') id: string) {
     return this.commentsService.delete(id);
   }
